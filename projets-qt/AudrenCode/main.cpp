f363d75ad7246079c695be605dad2f68b1beb8d9
Audren <audren.METERY-DROUIN@etu.univ-amu.fr>
2025-12-09 15:29:26 +0100

    rien

--- projets-qt/AudrenCode/main.cpp
+++ projets-qt/AudrenCode/main.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
+#include <vector>
 #include <QThread>
 
 using namespace std;
 
+typedef vector <unsigned> line; // un type représentant une ligne de la grille
+typedef vector <line> mat; // un type représentant la grille
+struct maPosition {
+    unsigned abs;
+    unsigned ord;
+};
+
+// QThread::msleep(500);
+
+// On efface tout l'écran
+void clearScreen () {
+    cout << "\033[H\033[2J";
+}
+
+void displayGrid() {
+
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+
     return 0;
 }
