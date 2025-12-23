3f302faa73304ae8381ec6db501062289537cdc8
Trohten2 <audren.meterydrouin@gmail.com>
2025-12-23 17:38:01 +0100

    maj

--- projet-en-cours/CandyCrush/main.cpp
+++ projet-en-cours/CandyCrush/main.cpp
@@ -23,14 +23,56 @@ void TestGridAffichage() {
 
     initGrid(Tableau, Taille);
     initEtatGrid(TableauEtat, Taille);
-
-    PositionActuelle.abs = 6;
-    PositionActuelle.ord = 3;
-
-    StatusDuJeu = MOUVEMENT;
-    Details = LOW;
-
     displayGrid(Tableau, TableauEtat, StatusDuJeu, PositionActuelle, Details);
+
+    while (true) {
+        //-------------------------- script TEMPORAIRE (sera remplacer par le control.h)
+        //------ On choisis la position
+        string tmp;
+        // On choisis l'abscisse
+        cout << "choisis abs" << endl;
+        StatusDuJeu = ABS;
+        displayGrid(Tableau, TableauEtat, StatusDuJeu, PositionActuelle, Details);
+        cin >> tmp;
+        PositionActuelle.abs = stoi(tmp);
+
+        // On choisis l'ordonnée
+        cout << "choisis ord" << endl;
+        StatusDuJeu = ORD;
+        displayGrid(Tableau, TableauEtat, StatusDuJeu, PositionActuelle, Details);
+        cin >> tmp;
+        PositionActuelle.abs = stoi(tmp);
+
+        //------ On choisis la direction
+        StatusDuJeu = MOUVEMENT;
+
+        //------ On échange les bonbons
+
+        //-------------------------- On élimine les chaînes de bonbons et on applique la gravité
+        //------ Première position
+        unsigned int howManyColumn;
+        bool columnFound = atLeastThreeInAColumn(Tableau, PositionActuelle, howManyColumn);
+        unsigned int howManyRow;
+        bool rowFound = atLeastThreeInARow(Tableau, PositionActuelle, howManyRow);
+        if (columnFound && rowFound) {
+            // Ligne et colonne trouvé en même temps, on élimine donc la chaîne la plus longue
+            if (howManyColumn >= howManyRow) {
+                // On élimine la colonne
+                removalInColumn(Tableau, TableauEtat, PositionActuelle, howManyColumn);
+            } else {
+                // On élimine la ligne
+                removalInRow(Tableau, TableauEtat, PositionActuelle, howManyRow);
+            }
+        } else if (columnFound) {
+            // On élimine la colonne
+            removalInColumn(Tableau, TableauEtat, PositionActuelle, howManyColumn);
+        } else if (rowFound) {
+            // On élimine la ligne
+            removalInRow(Tableau, TableauEtat, PositionActuelle, howManyRow);
+        }
+
+    }
+
 }
 
 int main()
